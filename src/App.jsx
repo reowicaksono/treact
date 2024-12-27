46b8b2850647e75e0a8a9a8ec3c6d00ca71175df
reowicaksono <reo.wicaksono.2005356@gmail.com>
2024-12-27 17:35:37 +0700

    restructure architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/dashboard";
-import Login from "./pages/login";
+import Login from "./module/login/login";
+import Dashboard from "./module/dashboard/dashboard";
+
 
 export default function App() {
   return (
    <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      
    </Routes>
   )
 }
