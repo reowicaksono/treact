e65c64e2136ab5a7fe6119e280f47e0a725b067d
reowicaksono <reo.wicaksono.2005356@gmail.com>
2024-12-30 10:48:13 +0700

    re design login code

--- src/module/scan-qr/scan-qr.jsx
+++ src/module/scan-qr/scan-qr.jsx
@@ -24,33 +24,40 @@ function ScanQR() {
     }
     
     return (
-        <div
-            className='text-white'
-            style={{
-                backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                minHeight: '100vh'
-            }}
-        >
-            <div className='d-flex justify-content-center'>
-                <div className="mt-4 video-qr-code">
-                    {
-                        camera_permission
-                        &&
-                        <BarcodeScannerComponent
-                            width={500}
-                            height={500}
-                            onUpdate={(err, result) => {
-                                if (result) navigateToLoginPage(result.text);
-                                else setLoginCode("Cannot read login code")
-                            }}
-                        />
-                    }
-                    <p className='text-center'>
-                        Arahkan QR Code kedalam bingkai
-                    </p>
+       <div className='h-screen flex flex-col justify-center items-center bg-blue'>
+           <div className='bg-slate-50 mt-10 p-5 rounded-lg shadow-xl'>
+                <h1 className='font-bold text-4xl'>QR CODE</h1>
+
+                <div className='flex flex-row gap-4 mt-5'>
+                    <div className='flex flex-col '>
+                        <h1 className='font-bold text-2xl'>{login_code}</h1>
+                        <p>Step 1: Scan QR Code</p>
+                        <p>Step 2: Login</p>
+                    </div>
+                    <div>
+                        {
+                            camera_permission
+                            &&
+                            <BarcodeScannerComponent
+                                width={500}
+                                height={500}
+                                onUpdate={(err, result) => {
+                                    if (result) navigateToLoginPage(result.text);
+                                    else setLoginCode("Cannot read login code")
+                                }}
+                            />
+                        }
+                    </div>
+                    
                 </div>
-            </div>
-        </div>
+                {/* or login */}
+                <div className='flex flex-col gap-4 mt-5 justify-center items-center'>
+                    <p>Or</p>
+                    <button className='bg-blue-500 text-white p-2 rounded-lg' onClick={() => navigateToLoginPage("/login") }>Login</button>
+                </div>
+           </div>
+
+       </div>
     )
 }
 
